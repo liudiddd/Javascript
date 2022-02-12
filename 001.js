5f077d55eec4f8881161d113f3622e15442f2dad
adee <609980188@qq.com>
2022-02-12 10:52:08 +0800

    init

---
+++ 001.js
@@ -0,0 +1,16 @@
+/*
+1.简单类型与复杂类型
+    简单数据类型又叫做值类型，复杂数据类型又叫做引用类型。
+    - 值类型
+        string number boolean undefined null 
+    - 引用类型
+        通过new关键字创建的对象（系统对象、自定义对象），如Object、Array、Date等。
+        
+*/
+
+// 简单数据类型null 返回是一个空对象 object 
+var timer = null 
+console.log(typeof null) // object 
+// 如果有个变量打算存储为对象，但暂时没想好放啥，这时候就给null
+
+
